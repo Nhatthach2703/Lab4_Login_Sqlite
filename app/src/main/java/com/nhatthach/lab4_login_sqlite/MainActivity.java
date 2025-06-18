8d446016d6b32334d124ac8908f164581bc14e3b
unknown <nhatthach2703@gmail.com>
2025-06-19 00:00:14 +0700

    Add Add function

--- app/src/main/java/com/nhatthach/lab4_login_sqlite/MainActivity.java
+++ app/src/main/java/com/nhatthach/lab4_login_sqlite/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     String TAG = "//=====";
     RecyclerView rcvTask;
     TaskInfoAdapter adapter;
-    Button btnList;
+    EditText edId, edTitle, edContent, edDate, edType;
+    Button btnAdd;
     public String username = "";
     public String password = "";
 
@@ -58,9 +62,63 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rcvTask.setLayoutManager(linearLayoutManager);
         rcvTask.setAdapter(adapter);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = edTitle.getText().toString().trim();
+                String content = edContent.getText().toString().trim();
+                String date = edDate.getText().toString().trim();
+                String type = edType.getText().toString().trim();
+
+                if (title.isEmpty() || content.isEmpty() || date.isEmpty() || type.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please input data", Toast.LENGTH_SHORT).show();
+                    if (title.isEmpty()) {
+                        edTitle.setError("Please enter title");
+                    }
+                    if (content.isEmpty()) {
+                        edContent.setError("Please enter content");
+                    }
+                    if (date.isEmpty()) {
+                        edDate.setError("Please enter date");
+                    }
+                    if (type.isEmpty()) {
+                        edType.setError("Please enter type");
+                    }
+                } else {
+                    TaskInfo info = new TaskInfo(1, title, content, date, type, 0);
+                    long check = dao.addInfo(info);
+                    if (check < 0) {
+                        Toast.makeText(MainActivity.this, "ERROR: Not insert data", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insert successful", Toast.LENGTH_SHORT).show();
+                    }
+                    listTask = dao.getListInfo();
+                    adapter = new TaskInfoAdapter(MainActivity.this, listTask);
+                    rcvTask.setLayoutManager(linearLayoutManager);
+                    rcvTask.setAdapter(adapter);
+                    reset();
+                }
+            }
+        });
     }
 
     public void initGUI() {
         rcvTask = findViewById(R.id.rcvTask);
+
+        edId = findViewById(R.id.edId);
+        edTitle = findViewById(R.id.edTitle);
+        edContent = findViewById(R.id.edContent);
+        edDate = findViewById(R.id.edDate);
+        edType = findViewById(R.id.edType);
+        btnAdd = findViewById(R.id.btnAdd);
+    }
+
+    public void reset() {
+        edId.setText("");
+        edTitle.setText("");
+        edContent.setText("");
+        edDate.setText("");
+        edType.setText("");
     }
 }
